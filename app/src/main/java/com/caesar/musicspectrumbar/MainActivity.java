353893f273d4843c006742e096d5491a11b09613
Caesar <15757855271@163.com>
2019-07-07 21:02:49 +0800

    例子优化

--- app/src/main/java/com/caesar/musicspectrumbar/MainActivity.java
+++ app/src/main/java/com/caesar/musicspectrumbar/MainActivity.java
@@ -2,6 +2,10 @@ package com.caesar.musicspectrumbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.caesar.musicspectrumbarlibrary.MusicSpectrumBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        MusicSpectrumBar mb1 = findViewById(R.id.mb_type1);
+        TextView tv_show = findViewById(R.id.tv_show);
+        Button btnOne = findViewById(R.id.btn_one);
+        Button btnTwo = findViewById(R.id.btn_two);
+        mb1.setOnSeekBarChangeListener(new MusicSpectrumBar.OnSeekChangeListener() {
+            @Override
+            public void onProgressChanged(int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch() {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(int i) {
+
+            }
+        });
+
     }
 }
